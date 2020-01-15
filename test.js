77d6d445d246c4d94289b61aef808cfca84e0fa1
Richie Bendall <richiebendall@gmail.com>
2020-01-15 21:53:05 +1300

    chore: Update deps
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- test.js
+++ test.js
@@ -4,43 +4,43 @@ import { encode } from "iconv-lite"
 import convertBody from "./src"
 
 test("should support encoding decode, xml dtd detect", (t) => {
-    const text = "<?xml version=\"1.0\" encoding=\"EUC-JP\"?><title>日本語</title>"
-    t.is(convertBody(encode(text, "EUC-JP"), new Headers({ "Content-Type": "text/xml" })), text)
+	const text = "<?xml version=\"1.0\" encoding=\"EUC-JP\"?><title>日本語</title>"
+	t.is(convertBody(encode(text, "EUC-JP"), new Headers({ "Content-Type": "text/xml" })), text)
 })
 
 test("should support encoding decode, content-type detect", (t) => {
-    const text = "<div>日本語</div>"
-    t.is(convertBody(encode(text, "Shift_JIS"), new Headers({ "Content-Type": "text/html; charset=Shift-JIS" })), text)
+	const text = "<div>日本語</div>"
+	t.is(convertBody(encode(text, "Shift_JIS"), new Headers({ "Content-Type": "text/html; charset=Shift-JIS" })), text)
 })
 
 test("should support encoding decode, html5 detect", (t) => {
-    const text = "<meta charset=\"gbk\"><div>中文</div>"
-    t.is(convertBody(encode(text, "gbk"), new Headers({ "Content-Type": "text/html" })), text)
+	const text = "<meta charset=\"gbk\"><div>中文</div>"
+	t.is(convertBody(encode(text, "gbk"), new Headers({ "Content-Type": "text/html" })), text)
 })
 
 test("should support encoding decode, html4 detect", (t) => {
-    const text = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=gb2312\"><div>中文</div>"
-    t.is(convertBody(encode(text, "gb2312"), new Headers({ "Content-Type": "text/html" })), text)
+	const text = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=gb2312\"><div>中文</div>"
+	t.is(convertBody(encode(text, "gb2312"), new Headers({ "Content-Type": "text/html" })), text)
 })
 
 test("should default to utf8 encoding", (t) => {
-    const text = "中文"
-    t.is(convertBody(text), text)
+	const text = "中文"
+	t.is(convertBody(text), text)
 })
 
 test("should support uncommon content-type order, end with qs", (t) => {
-    const text = "中文"
-    t.is(convertBody(encode(text, "gbk"), new Headers({ "Content-Type": "text/plain; charset=gbk; qs=1" })), text)
+	const text = "中文"
+	t.is(convertBody(encode(text, "gbk"), new Headers({ "Content-Type": "text/plain; charset=gbk; qs=1" })), text)
 })
 
 test("should support chunked encoding, html4 detect", (t) => {
-    const text = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=Shift_JIS\" /><div>日本語</div>"
-    const padding = "a".repeat(10)
-    t.is(convertBody(encode(padding + text, "Shift_JIS"), new Headers({ "Content-Type": "text/html", "Transfer-Encoding": "chunked" })), padding + text)
+	const text = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=Shift_JIS\" /><div>日本語</div>"
+	const padding = "a".repeat(10)
+	t.is(convertBody(encode(padding + text, "Shift_JIS"), new Headers({ "Content-Type": "text/html", "Transfer-Encoding": "chunked" })), padding + text)
 })
 
 test("should only do encoding detection up to 1024 bytes", (t) => {
-    const text = "中文"
-    const padding = "a".repeat(1200)
-    t.not(convertBody(encode(padding + text, "gbk"), new Headers({ "Content-Type": "text/html", "Transfer-Encoding": "chunked" })), text)
+	const text = "中文"
+	const padding = "a".repeat(1200)
+	t.not(convertBody(encode(padding + text, "gbk"), new Headers({ "Content-Type": "text/html", "Transfer-Encoding": "chunked" })), text)
 })
